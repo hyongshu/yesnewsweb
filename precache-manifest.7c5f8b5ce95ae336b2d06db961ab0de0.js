6aaf8e8500a1ebc9311aaf331ea40f87811f55d6
Anusit <hyongshu@gmail.com>
2020-05-04 00:01:27 +0700

    Updates

---
+++ precache-manifest.7c5f8b5ce95ae336b2d06db961ab0de0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28985723a81561d937f2022dcb81995b",
+    "url": "/yesnewsweb/index.html"
+  },
+  {
+    "revision": "6b82e251828d23efc9b7",
+    "url": "/yesnewsweb/static/css/main.071d8002.chunk.css"
+  },
+  {
+    "revision": "44a011eb961eaa024e69",
+    "url": "/yesnewsweb/static/js/2.744a96ca.chunk.js"
+  },
+  {
+    "revision": "0b08544f54c9906705be39c03b7e79ad",
+    "url": "/yesnewsweb/static/js/2.744a96ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b82e251828d23efc9b7",
+    "url": "/yesnewsweb/static/js/main.7c130140.chunk.js"
+  },
+  {
+    "revision": "8765afbd9930d037df4b",
+    "url": "/yesnewsweb/static/js/runtime-main.4146c0e3.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.07b7a10dfc8a222c5d36330613bb375a.js"
+  "/yesnewsweb/precache-manifest.7c5f8b5ce95ae336b2d06db961ab0de0.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/yesnewsweb/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
