de1509f49a8a42b2d1e14a2c7525149d4cda59b5
Anusit <hyongshu@gmail.com>
2020-05-03 21:39:30 +0700

    Updates

---
+++ precache-manifest.c4b1dfbbd33fb33aabb4cfaae786f240.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41e8ce46a90b68f626015954938c8422",
+    "url": "/yesnewsweb/index.html"
+  },
+  {
+    "revision": "79b0f890e2bd0ca43df3",
+    "url": "/yesnewsweb/static/css/main.071d8002.chunk.css"
+  },
+  {
+    "revision": "6f254ed1e33fd9c031b5",
+    "url": "/yesnewsweb/static/js/2.4c44cf34.chunk.js"
+  },
+  {
+    "revision": "0b08544f54c9906705be39c03b7e79ad",
+    "url": "/yesnewsweb/static/js/2.4c44cf34.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79b0f890e2bd0ca43df3",
+    "url": "/yesnewsweb/static/js/main.7ea5e264.chunk.js"
+  },
+  {
+    "revision": "8765afbd9930d037df4b",
+    "url": "/yesnewsweb/static/js/runtime-main.4146c0e3.js"
+  }
+]);
