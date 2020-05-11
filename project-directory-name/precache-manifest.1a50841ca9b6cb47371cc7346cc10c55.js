89cea07bf161310651b9c758025db32241bc8c2c
Anusit Chamornmano <anusit@Anusits-MacBook-Pro.local>
2020-05-11 16:10:36 +0700

    Updates

---
+++ project-directory-name/precache-manifest.1a50841ca9b6cb47371cc7346cc10c55.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f35ce5515352f2f82a1bab371e8c802",
+    "url": "/yesnewsweb/index.html"
+  },
+  {
+    "revision": "d25c9de9878e928a0577",
+    "url": "/yesnewsweb/static/css/2.13034bdc.chunk.css"
+  },
+  {
+    "revision": "73d4dc0891e522f46223",
+    "url": "/yesnewsweb/static/css/main.4f29362c.chunk.css"
+  },
+  {
+    "revision": "d25c9de9878e928a0577",
+    "url": "/yesnewsweb/static/js/2.19c57a9a.chunk.js"
+  },
+  {
+    "revision": "b6b8f45a5c542a25054e5fef46a295f2",
+    "url": "/yesnewsweb/static/js/2.19c57a9a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73d4dc0891e522f46223",
+    "url": "/yesnewsweb/static/js/main.0858835f.chunk.js"
+  },
+  {
+    "revision": "8765afbd9930d037df4b",
+    "url": "/yesnewsweb/static/js/runtime-main.4146c0e3.js"
+  }
+]);
